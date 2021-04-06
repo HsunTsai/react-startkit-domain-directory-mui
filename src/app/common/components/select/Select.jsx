a089609e139678cdda61e800cbd407f6cc382da0
Hsun_windows_10 <t100310414@ntut.org.tw>
2021-04-06 22:33:26 +0800

    feat(Select): add default placeholder

--- src/app/common/components/select/Select.jsx
+++ src/app/common/components/select/Select.jsx
@@ -3,17 +3,24 @@ import PropTypes from 'prop-types';
 import { FormControl, Select, MenuItem } from '@material-ui/core';
 
 const CustomSelect = props => {
-	const { options, ...prop } = props;
+	const { options, placeholder, ...prop } = props;
 	return (
 		<FormControl variant="outlined">
-			{/* eslint-disable-next-line react/jsx-props-no-spreading */}
-			<Select {...prop}>
+			<Select displayEmpty {...prop}>
+				{placeholder && (
+					<MenuItem disabled value="">
+						<div style={{ color: '#AAA', fontSize: '1rem' }}>{placeholder}</div>
+					</MenuItem>
+				)}
 				{Array.isArray(options) &&
-					options.map(({ label, value }) => (
-						<MenuItem key={value} value={value}>
-							{label}
-						</MenuItem>
-					))}
+					options.map(option => {
+						const { label, value } = typeof option === 'object' ? option : { label: option, value: option };
+						return (
+							<MenuItem key={value} value={value}>
+								{label}
+							</MenuItem>
+						);
+					})}
 			</Select>
 		</FormControl>
 	);
@@ -21,10 +28,12 @@ const CustomSelect = props => {
 
 CustomSelect.defaultProps = {
 	options: [],
+	placeholder: undefined,
 };
 
 CustomSelect.propTypes = {
 	options: PropTypes.arrayOf(PropTypes.any),
+	placeholder: PropTypes.string,
 };
 
 export default CustomSelect;
